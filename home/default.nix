66bfc7d07a6469bb26633d5fd4b9debaa01f6fcd
Yukii <jhtdesu@gmail.com>
2026-07-22 09:55:03 +0700

    updated to 26.05

--- home/default.nix
+++ home/default.nix
@@ -37,6 +37,7 @@
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+    gtk4.theme = null;
   };
 
   home.sessionVariables = {
--- home/git.nix
+++ home/git.nix
@@ -14,7 +14,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."github.com" = {
+    settings."github.com" = {
       hostname = "github.com";
       identityFile = "~/.ssh/id_ed25519_github";
     };
